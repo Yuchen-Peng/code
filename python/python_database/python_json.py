ff229a31b0dba34968bd611d970f0542a69781dd
peng19880925 <pengpyc@gmail.com>
2017-07-07 16:09:23 -0400

    Update python_json.py

--- python/python_database/python_json.py
+++ python/python_database/python_json.py
@@ -1,3 +1,13 @@
 '''
 JSON is basically a list of lists/dicts. Use package json to read JSON by Python
 '''
+
+import json
+import sqlite3
+
+try:
+  str_data = open('./data.json').read() # read the whole .json file into memory
+except:
+  raise Exception('Error in read in the file')
+json_data = json.loads(str_data)
+# now we can loop through json_data for each entry
