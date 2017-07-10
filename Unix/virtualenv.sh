8b46ace085db83aa5379f6f57e1206f8258b7aa2
peng19880925 <pengpyc@gmail.com>
2017-07-10 17:20:36 -0400

    Create virtualenv.sh

---
+++ Unix/virtualenv.sh
@@ -0,0 +1,9 @@
+conda create -n virtualenv python=3.6 anaconda
+
+source activate virtualenv
+
+conda install -n virtualenv [package_name] # to install new package in virtualenv
+
+source deactivate # to deactivate
+
+
