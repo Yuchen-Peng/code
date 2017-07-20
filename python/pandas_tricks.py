33d53e6ec7e43093d3a6b686e4c9f42affff67c9
peng19880925 <pengpyc@gmail.com>
2017-07-20 18:17:18 -0400

    Update pandas_tricks.py

--- python/pandas_tricks.py
+++ python/pandas_tricks.py
@@ -5,3 +5,10 @@ df_groupby = df.groupby([col1, col2]).agg(['mean', 'count','std', 'min', 'max'])
 import math
 x=float('nan')
 math.isnan(x)
+
+# applying pandas qcut bins determined from a benchmark data to new data
+ser, bins = pd.qcut(df_ben["A"], 5, retbins=True, labels=False) # here bins is a 6-length array label the bin edges
+pd.cut(df_new["A"], bins=bins, labels=False, include_lowest=True)
+
+# count in bins
+bin_count = pd.qcut(df_ben["A"], 5).value_counts
