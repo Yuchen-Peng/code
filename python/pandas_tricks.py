2a9f8cf78956d25cf822000474fe11dd8984d04a
peng19880925 <pengpyc@gmail.com>
2017-07-20 16:30:25 -0400

    Update pandas_tricks.py

--- python/pandas_tricks.py
+++ python/pandas_tricks.py
@@ -1,2 +1,7 @@
 # create a dataframe which contains multiple groupby
 df_groupby = df.groupby([col1, col2]).agg(['mean', 'count','std', 'min', 'max'])
+
+# check float type nan
+import math
+x=float('nan')
+math.isnan(x)
