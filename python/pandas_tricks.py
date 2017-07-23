dac2d3d19528528a747f07605c2f3be27ff8ec95
peng19880925 <pengpyc@gmail.com>
2017-07-23 09:20:23 -0400

    Update pandas_tricks.py

--- python/pandas_tricks.py
+++ python/pandas_tricks.py
@@ -39,3 +39,7 @@ Out[12]:
 7      NaN
 8      0.0
 dtype: float64
+ 
+ # sum over multiple column
+ col_list_2015=list(df_houseprice[['2015-01','2015-02','2015-03','2015-04','2015-05','2015-06','2015-07','2015-08','2015-09','2015-10','2015-11','2015-12']])
+ df_houseprice['avg_price_2015'] = 1/12*df_houseprice_141516[col_list_2015].sum(axis=1)
