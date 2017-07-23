e279570491c8be75d44fc6130a3f36749ae5dad0
peng19880925 <pengpyc@gmail.com>
2017-07-23 09:18:46 -0400

    Update pandas_tricks.py

--- python/pandas_tricks.py
+++ python/pandas_tricks.py
@@ -12,3 +12,30 @@ pd.cut(df_new["A"], bins=bins, labels=False, include_lowest=True)
 
 # count in bins
 bin_count = pd.qcut(df_ben["A"], 5).value_counts()
+
+# object / str missing to float
+ s
+Out[11]: 
+0    103.8
+1    751.1
+2      0.0
+3      0.0
+4        -
+5        -
+6      0.0
+7        -
+8      0.0
+dtype: object
+
+s.convert_objects(convert_numeric=True)
+Out[12]: 
+0    103.8
+1    751.1
+2      0.0
+3      0.0
+4      NaN
+5      NaN
+6      0.0
+7      NaN
+8      0.0
+dtype: float64
