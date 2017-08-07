1bf68de157048cbd7705fa9e2a587771cf8a6c68
peng19880925 <pengpyc@gmail.com>
2017-08-07 11:26:20 -0400

    Create string_tricks.py

---
+++ python/string_tricks.py
@@ -0,0 +1,6 @@
+# use % for format
+print 'Hello %s, my name is %s, my age is %i, and my favorite number is %f' %('John', 'Mike', 10, 3.14)   
+
+a = 'hahaha'
+b = 'lalala'
+x = "this file contains: %s, %s" %(a,b)
