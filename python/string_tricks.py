6dd12dc794d774a4a850b060a7fb5003c3cec7b4
peng19880925 <pengpyc@gmail.com>
2017-08-07 11:27:18 -0400

    Update string_tricks.py

--- python/string_tricks.py
+++ python/string_tricks.py
@@ -2,7 +2,6 @@
 print 'Hello %s, my name is %s, my age is %i, and my favorite number is %f' %('John', 'Mike', 10, 3.14) 
 >>> Hello John, my name is Mike, my age is 10, and my favorite number is 3.140000
 
-
 a = 'hahaha'
 b = 'lalala'
 x = "this file contains: %s, %s" %(a,b)
