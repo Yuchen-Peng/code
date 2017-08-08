21c8aa229934f6c48d9f6a0e233e894810f65eb4
peng19880925 <pengpyc@gmail.com>
2017-08-08 17:15:01 -0400

    Update conda_environment_setup.sh

--- Unix/conda_environment_setup.sh
+++ Unix/conda_environment_setup.sh
@@ -1,8 +1,8 @@
 
-conda create -n dp_env python=3.5 bokeh seaborn jupyter ipywidgets pyodbc teradata pandas pyodbc python-dateutil 
+conda create -n dp_env3 python=3.5 bokeh seaborn jupyter ipywidgets pyodbc teradata pandas pyodbc python-dateutil 
 #Python 3.5
 
-source activate dp_env
+source activate dp_env3
 #only work in bash; to enter dp_environment
 #and add /prod/user/sam/$LOB/$USER/python/conda_envs/dp_env/bin to PATH in .bashrc
 #in this environment packages can be added easily.
