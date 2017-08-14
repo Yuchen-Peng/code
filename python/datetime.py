ab6fdd862247dbdbaf3e985e2c790bad3a92e0fb
peng19880925 <pengpyc@gmail.com>
2017-08-14 15:27:48 -0400

    Update datetime.py

--- python/datetime.py
+++ python/datetime.py
@@ -4,6 +4,10 @@ datetime(2012, 5, 1)
 # string to datetime
 datetime.datetime.strptime('20170430',"%Y%m%d")
 
+# datetime to string
+current = datetime.datetime(2017, 7, 31, 0, 0)
+current_str = current.strftime("%Y-%m-%d") 
+
 # add month
 current = datetime.datetime(mydate.year, mydate.month, 1)
-six_month_later = datetime.datetime(mydate.year - (mydate.month<6), ((mydate.month-6 ) %12 + 12*(mydate.month==6)), 1)
+six_month_before = datetime.datetime(mydate.year - (mydate.month<6), ((mydate.month-6 ) %12 + 12*(mydate.month==6)), 1)
