150b29e936f3e424b2b89b4b2a377ce14ae3812f
peng19880925 <pengpyc@gmail.com>
2017-08-28 12:21:27 -0400

    Update matplot.py

--- python/matplot.py
+++ python/matplot.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
+%matplotlib inline # always include this in a notebook to display figures in the notebook
+
 #plot function
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2*np.pi*t)
@@ -10,3 +12,5 @@ plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
 plt.grid(True)
 plt.show()
+
+#plot in a loop
