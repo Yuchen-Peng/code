baab3c6e676eb78aa5c65ff91aa8c7f897c0cac3
peng19880925 <pengpyc@gmail.com>
2017-09-27 17:26:19 -0400

    Update numpy_tricks.py

--- python/numpy_tricks.py
+++ python/numpy_tricks.py
@@ -1,3 +1,9 @@
 # concatenate two arrays or two matrices by column
 np.column_stack((array1, array2))
 
+# create matrix
+Matrix = [[0 for x in range(width)] for y in range(height)]
+for i in range(len(height)):
+    for j in range(len(width)):
+        Matrix[j][i] = blablabla
+
