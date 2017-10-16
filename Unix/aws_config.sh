3307b1cb5b7d7ca3edbd87e7bdf72d42c22ea85f
peng19880925 <pengpyc@gmail.com>
2017-10-16 13:53:21 -0400

    Update aws_config.sh

--- Unix/aws_config.sh
+++ Unix/aws_config.sh
@@ -19,9 +19,9 @@ sudo yum install nano
 
 #anaconda
 #check https://repo.continuum.io/archive/index.html
-wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 
-bash Anaconda3-4.0.0-Linux-x86_64.sh
+bash Anaconda3-4.3.0-Linux-x86_64.sh
 
 source ~/.bashrc
 
