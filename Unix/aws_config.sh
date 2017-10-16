758c6d7e890ed9fc259782827bcc9ce479877f12
peng19880925 <pengpyc@gmail.com>
2017-10-16 15:31:52 -0400

    Update aws_config.sh

--- Unix/aws_config.sh
+++ Unix/aws_config.sh
@@ -45,7 +45,7 @@ pip install requests
 pip install tabulate
 pip install scikit-learn --upgrade
 pip install matplotlib --upgrade
-pip install seaborn
+pip install seaborn --upgrade
 
 #h2o (add --user after pip might work if other breaks down, e.g. pip install package --user)
 #other learning tools
