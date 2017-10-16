538f4be726262544d369462724d734babfde7580
peng19880925 <pengpyc@gmail.com>
2017-10-16 15:50:41 -0400

    Update aws_config.sh

--- Unix/aws_config.sh
+++ Unix/aws_config.sh
@@ -53,4 +53,4 @@ pip install seaborn --upgrade
 pip install http://h2o-release.s3.amazonaws.com/h2o/rel-ueno/1/Python/h2o-3.10.4.1-py2.py3-none-any.whl
 pip install h2o --upgrade
 python3 -m pip install tensorflow # tensorflow
-conda install pymc3 #py bayesian
+pip install pymc3 --upgrade #py bayesian
