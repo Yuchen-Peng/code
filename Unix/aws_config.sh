133065cc4f9613384d8f5dc60518ed398bdd2814
peng19880925 <pengpyc@gmail.com>
2017-10-25 21:32:33 -0400

    Update aws_config.sh

--- Unix/aws_config.sh
+++ Unix/aws_config.sh
@@ -18,6 +18,8 @@ sudo yum install bzip2
 sudo yum install nano
 sudo yum install mesa-libGL.x86_64 # for matplotlib.pyplot
 
+sudo yum upgrade
+
 #anaconda
 #check https://repo.continuum.io/archive/index.html
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
